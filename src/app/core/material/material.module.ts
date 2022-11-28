a00a0ea3522a152710dc3f6482508c213178b6d5
Johannes Maierhofer <johannes.maierhofer@argo-soft.com>
2022-11-28 13:19:23 +0100

    material.module: import/export all Material modules

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -4,8 +4,10 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -13,25 +15,31 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+const materialModules = [
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatBadgeModule,
+  MatPaginatorModule,
+  MatGridListModule,
+  MatCardModule,
+  MatTooltipModule,
+  MatFormFieldModule,
+  MatInputModule
+];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...materialModules
   ],
   exports: [
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    MatListModule,
-    MatSidenavModule,
-    MatSnackBarModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatBadgeModule,
-    MatPaginatorModule,
-    MatGridListModule,
-    MatCardModule,
-    MatTooltipModule
+    ...materialModules
   ]
 })
 export class MaterialModule { }
